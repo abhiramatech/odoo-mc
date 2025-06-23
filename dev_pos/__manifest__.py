b2476a4041d6cf8f48075d84d598b0df945f3d44
Zulfa Istiani <zulfafani@gmail.com>
2025-06-23 11:32:42 +0700

    test

--- dev_pos/__manifest__.py
+++ dev_pos/__manifest__.py
@@ -60,7 +60,7 @@
         'views/print_barcode_view.xml',
         'views/product_product_view.xml',
         'views/mrp_unbuild_line_view.xml',
-        'reports/sales_report_view.xml',
+        # 'reports/sales_report_view.xml',
     ],
     'assets': {
         'web.assets_backend': [
