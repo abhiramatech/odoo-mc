c12954a1e3b47e393400116b4e2cf6473bee0b29
Zulfa Istiani <zulfafani@gmail.com>
2025-06-23 11:28:04 +0700

    test

--- dev_pos/reports/sales_report.py
+++ dev_pos/reports/sales_report.py
@@ -7,7 +7,7 @@ class SalesReport(models.TransientModel):
 
     vit_date_from = fields.Date(string='Date From')
     vit_date_to = fields.Date(string='Date To')
-    vit_invoice_no = fields.Char(string='Invoice No', required=True)
+    vit_invoice_no = fields.Char(string='Invoice No')
     vit_sales_detail = fields.Boolean(string="Sales Report Detail", default=False)
     vit_sales_recap = fields.Boolean(string="Sales Report Recap ", default=False)
     vit_sales_spending = fields.Boolean(string="Sales Report Spending", default=False)
