30c09d8aca213bd901addd690e4d2dcbc63679de
Zulfa Istiani <zulfafani@gmail.com>
2025-06-23 14:54:44 +0700

    test

--- dev_pos/reports/sales_report.py
+++ dev_pos/reports/sales_report.py
@@ -8,10 +8,19 @@ class SalesReport(models.TransientModel):
     vit_date_from = fields.Date(string='Date From')
     vit_date_to = fields.Date(string='Date To')
     vit_invoice_no = fields.Char(string='Invoice No')
-    vit_sales_detail = fields.Boolean(string="Sales Report Detail", default=False)
-    vit_sales_recap = fields.Boolean(string="Sales Report Recap ", default=False)
-    vit_sales_spending = fields.Boolean(string="Sales Report Spending", default=False)
-    vit_sales_hourly = fields.Boolean(string="Sales Report Hourly", default=False)
+    # vit_sales_detail = fields.Boolean(string="Sales Report Detail", default=False)
+    # vit_sales_recap = fields.Boolean(string="Sales Report Recap ", default=False)
+    # vit_sales_spending = fields.Boolean(string="Sales Report Spending", default=False)
+    # vit_sales_hourly = fields.Boolean(string="Sales Report Hourly", default=False)
     
-    # def action_generate_report(self):
-    #     return
+    def action_generate_report_detail(self):
+        raise ValidationError("Masuk action_generate_report_detail")
+
+    def action_generate_report_hourly(self):
+        raise ValidationError("Masuk action_generate_report_hourly")
+    
+    def action_generate_report_recap(self):
+        raise ValidationError("Masuk action_generate_report_recap")
+    
+    def action_generate_report_spending(self):
+        raise ValidationError("Masuk action_generate_report_spending")
