ab863bc658a85cd95314d918aa7c5b887e54209a
Zulfa Istiani <zulfafani@gmail.com>
2025-06-23 15:40:23 +0700

    test

--- dev_pos/reports/sales_report.py
+++ dev_pos/reports/sales_report.py
@@ -1,17 +1,13 @@
 from odoo import models, fields, _, api
 from odoo.exceptions import UserError, ValidationError
 
-class SalesReport(models.TransientModel):
+class SalesReportDetail(models.TransientModel):
     _name = 'sales.report'
     _description = 'Sales Report'
 
     vit_date_from = fields.Date(string='Date From')
     vit_date_to = fields.Date(string='Date To')
     vit_invoice_no = fields.Char(string='Invoice No')
-    # vit_sales_detail = fields.Boolean(string="Sales Report Detail", default=False)
-    # vit_sales_recap = fields.Boolean(string="Sales Report Recap ", default=False)
-    # vit_sales_spending = fields.Boolean(string="Sales Report Spending", default=False)
-    # vit_sales_hourly = fields.Boolean(string="Sales Report Hourly", default=False)
     
     def action_generate_report_detail(self):
         raise ValidationError("Masuk action_generate_report_detail")
