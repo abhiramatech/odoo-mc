d608a261ae1dbb32840e0c7c21c62f237d81ee71
Zulfa <zulfafani@gmail.com>
2025-08-06 16:06:34 +0700

    [TEST]

--- dev_pos/reports/sales_report_preview.py
+++ dev_pos/reports/sales_report_preview.py
@@ -5,8 +5,8 @@ from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
 class SalesReportDetailPreview(models.TransientModel):
     _inherit = 'sales.report'
  
-    def action_generate_report_detail_preview(self):
-        raise ValidationError(_(f"action_generate_report_detail_preview"))
+    def action_preview_report_detail(self):
+        # raise ValidationError(_(f"action_generate_report_detail_preview"))
         self.ensure_one()
         return {
             'type': 'ir.actions.act_url',
