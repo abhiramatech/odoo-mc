c213d9c864bc34d357d0e21d34d6a5a09a615919
Zulfa <zulfafani@gmail.com>
2025-08-06 15:51:43 +0700

    [TEST] preview report

---
+++ dev_pos/reports/sales_report_preview.py
@@ -0,0 +1,15 @@
+from odoo import models, fields, _, api
+from odoo.exceptions import UserError, ValidationError
+from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
+
+class SalesReportDetailPreview(models.TransientModel):
+    _inherit = 'sales.report'
+ 
+    def action_generate_report_detail_preview(self):
+        # raise ValidationError(_(f"action_generate_report_detail_preview"))
+        self.ensure_one()
+        return {
+            'type': 'ir.actions.act_url',
+            'target': 'self',
+            'url': self.get_portal_url(),
+        }
