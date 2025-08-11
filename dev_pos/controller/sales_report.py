791edef1aaed64f2468887a8ffc36156a8181a48
Zulfa <zulfafani@gmail.com>
2025-08-11 17:11:25 +0700

    test preview report detail

--- dev_pos/controller/sales_report.py
+++ dev_pos/controller/sales_report.py
@@ -1,30 +1,30 @@
-from odoo import http
+from odoo import http, fields
 from odoo.http import request
-from odoo import models, fields, _, api
 
 
 class SalesReportDetailController(http.Controller):
-    @http.route('/sales/report/detail/<int:wizard_id>', type='http', auth='user', website=True)
-    def portal_sales_report_detail(self, wizard_id, **kw):
-        wizard = request.env['sales.report'].sudo().browse(wizard_id)
-        if not wizard.exists():
+
+    @http.route('/sales/report/detail', type='http', auth='user', website=True)
+    def portal_sales_report_detail(self, **kw):
+        date_from = kw.get('date_from')
+        date_to = kw.get('date_to')
+        invoice_no = kw.get('invoice_no')
+        pos_order_ref = kw.get('pos_order_ref')
+
+        # Validasi parameter wajib
+        if not date_from or not date_to:
             return request.not_found()
 
-        # Ambil ulang data seperti sebelumnya
-        date_from = wizard.vit_date_from
-        date_to = wizard.vit_date_to
-        invoice_no = wizard.vit_invoice_no
-        pos_order_ref = wizard.vit_pos_order_ref
+        domain = [
+            ('date_order', '>=', date_from),
+            ('date_order', '<=', date_to),
+        ]
 
-        domain = []
-        if date_from:
-            domain.append(('date_order', '>=', date_from))
-        if date_to:
-            domain.append(('date_order', '<=', date_to))
         if invoice_no:
             move = request.env['account.move'].sudo().search([('name', '=', invoice_no)], limit=1)
             if move:
                 domain.append(('account_move', '=', move.id))
+
         if pos_order_ref:
             domain.append(('name', '=', pos_order_ref))
 
@@ -32,9 +32,8 @@ class SalesReportDetailController(http.Controller):
 
         values = {
             'orders': orders,
-            'date_from': date_from.strftime('%d/%m/%Y') if date_from else '',
-            'date_to': date_to.strftime('%d/%m/%Y') if date_to else '',
+            'date_from': fields.Date.from_string(date_from).strftime('%d/%m/%Y'),
+            'date_to': fields.Date.from_string(date_to).strftime('%d/%m/%Y'),
             'tanggal_cetak': fields.Date.today().strftime("%d %b %Y"),
         }
-
         return request.render('nama_modul.report_sales_detail', values)
