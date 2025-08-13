81a2bc6a60175f8c9bb53658927766ffb767be6c
Zulfa <zulfafani@gmail.com>
2025-08-13 16:31:37 +0700

    test

--- dev_pos/controller/sales_report.py
+++ dev_pos/controller/sales_report.py
@@ -105,9 +105,43 @@ class SalesReportDetailController(http.Controller):
         )
         tanggal_list = sorted(list(tanggal_set))
 
+        # Range Spending seperti di Python XLSX kamu
+        range_spending = [
+            (0, 50000, "0 - 50.000"),
+            (50001, 100000, "50.001 - 100.000"),
+            (100001, 250000, "100.001 - 250.000"),
+            (250001, 500000, "250.001 - 500.000"),
+            (500001, 1000000, "500.001 - 1.000.000"),
+            (1000001, 3000000, "1.000.001 - 3.000.000"),
+            (3000001, 5000000, "3.000.001 - 5.000.000"),
+            (5000001, 20000000, "5.000.001 - 20.000.000"),
+            (20000001, float('inf'), ">20.000.001"),
+        ]
+
+        # Bentuk data_rows
+        data_rows = []
+        for min_spending, max_spending, label in range_spending:
+            row_data = {'label': label}
+            for tgl in tanggal_list:
+                order_filtered = orders.filtered(lambda o:
+                    fields.Datetime.context_timestamp(request.env.user, o.date_order).date() == tgl and
+                    min_spending <= o.amount_total <= max_spending
+                )
+
+                total_qty = sum(sum(line.qty for line in o.lines) for o in order_filtered)
+                total_trx = len(order_filtered)
+                total_sales = sum(o.amount_total for o in order_filtered)
+
+                row_data[f"{tgl.strftime('%d/%m/%Y')}_qty"] = total_qty or ''
+                row_data[f"{tgl.strftime('%d/%m/%Y')}_trx"] = total_trx or ''
+                row_data[f"{tgl.strftime('%d/%m/%Y')}_sales"] = "{:,.0f}".format(total_sales) if total_sales else ''
+            
+            data_rows.append(row_data)
+
         values = {
             'orders': orders,
             'tanggal_list': tanggal_list,  # dikirim ke QWeb
+            'data_rows': data_rows,
             'date_from': fields.Date.from_string(date_from).strftime('%d/%m/%Y'),
             'date_to': fields.Date.from_string(date_to).strftime('%d/%m/%Y'),
             'tanggal_cetak': fields.Date.today().strftime("%d %b %Y"),
