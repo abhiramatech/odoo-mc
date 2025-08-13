271d9671e60d311392c22c70d220706c253dfcde
Zulfa <zulfafani@gmail.com>
2025-08-13 13:59:56 +0700

    add report preview

--- dev_pos/controller/sales_report.py
+++ dev_pos/controller/sales_report.py
@@ -71,3 +71,214 @@ class SalesReportDetailController(http.Controller):
             'tanggal_cetak': fields.Date.today().strftime("%d %b %Y"),
         }
         return request.render('dev_pos.report_sales_recap', values)
+
+    @http.route('/sales/report/spending', type='http', auth='user', website=True)
+    def portal_sales_report_spending(self, **kw):
+        date_from = kw.get('date_from')
+        date_to = kw.get('date_to')
+        # invoice_no = kw.get('invoice_no')
+        # pos_order_ref = kw.get('pos_order_ref')
+
+        # Validasi parameter wajib
+        if not date_from or not date_to:
+            return request.not_found()
+
+        domain = [
+            ('date_order', '>=', date_from),
+            ('date_order', '<=', date_to),
+        ]
+
+        # if invoice_no:
+        #     move = request.env['account.move'].sudo().search([('name', '=', invoice_no)], limit=1)
+        #     if move:
+        #         domain.append(('account_move', '=', move.id))
+
+        # if pos_order_ref:
+        #     domain.append(('name', '=', pos_order_ref))
+
+        orders = request.env['pos.order'].sudo().search(domain)
+
+        values = {
+            'orders': orders,
+            'date_from': fields.Date.from_string(date_from).strftime('%d/%m/%Y'),
+            'date_to': fields.Date.from_string(date_to).strftime('%d/%m/%Y'),
+            'tanggal_cetak': fields.Date.today().strftime("%d %b %Y"),
+        }
+        return request.render('dev_pos.report_sales_spending', values)
+    
+    @http.route('/sales/report/hourly', type='http', auth='user', website=True)
+    def portal_sales_report_hourly(self, **kw):
+        date_from = kw.get('date_from')
+        date_to = kw.get('date_to')
+        # invoice_no = kw.get('invoice_no')
+        # pos_order_ref = kw.get('pos_order_ref')
+
+        # Validasi parameter wajib
+        if not date_from or not date_to:
+            return request.not_found()
+
+        domain = [
+            ('date_order', '>=', date_from),
+            ('date_order', '<=', date_to),
+        ]
+
+        # if invoice_no:
+        #     move = request.env['account.move'].sudo().search([('name', '=', invoice_no)], limit=1)
+        #     if move:
+        #         domain.append(('account_move', '=', move.id))
+
+        # if pos_order_ref:
+        #     domain.append(('name', '=', pos_order_ref))
+
+        orders = request.env['pos.order'].sudo().search(domain)
+
+        values = {
+            'orders': orders,
+            'date_from': fields.Date.from_string(date_from).strftime('%d/%m/%Y'),
+            'date_to': fields.Date.from_string(date_to).strftime('%d/%m/%Y'),
+            'tanggal_cetak': fields.Date.today().strftime("%d %b %Y"),
+        }
+        return request.render('dev_pos.report_sales_hourly', values)
+
+
+    @http.route('/sales/report/hourly_category', type='http', auth='user', website=True)
+    def portal_sales_report_hourly_category(self, **kw):
+        date_from = kw.get('date_from')
+        date_to = kw.get('date_to')
+        invoice_no = kw.get('invoice_no')
+        pos_order_ref = kw.get('pos_order_ref')
+
+        # Validasi parameter wajib
+        if not date_from or not date_to:
+            return request.not_found()
+
+        domain = [
+            ('date_order', '>=', date_from),
+            ('date_order', '<=', date_to),
+        ]
+
+        if invoice_no:
+            move = request.env['account.move'].sudo().search([('name', '=', invoice_no)], limit=1)
+            if move:
+                domain.append(('account_move', '=', move.id))
+
+        if pos_order_ref:
+            domain.append(('name', '=', pos_order_ref))
+
+        orders = request.env['pos.order'].sudo().search(domain)
+
+        values = {
+            'orders': orders,
+            'date_from': fields.Date.from_string(date_from).strftime('%d/%m/%Y'),
+            'date_to': fields.Date.from_string(date_to).strftime('%d/%m/%Y'),
+            'tanggal_cetak': fields.Date.today().strftime("%d %b %Y"),
+        }
+        return request.render('dev_pos.report_sales_hourly_category', values)
+
+
+    @http.route('/sales/report/hourly_payment', type='http', auth='user', website=True)
+    def portal_sales_report_hourly_payment(self, **kw):
+        date_from = kw.get('date_from')
+        date_to = kw.get('date_to')
+        invoice_no = kw.get('invoice_no')
+        pos_order_ref = kw.get('pos_order_ref')
+
+        # Validasi parameter wajib
+        if not date_from or not date_to:
+            return request.not_found()
+
+        domain = [
+            ('date_order', '>=', date_from),
+            ('date_order', '<=', date_to),
+        ]
+
+        if invoice_no:
+            move = request.env['account.move'].sudo().search([('name', '=', invoice_no)], limit=1)
+            if move:
+                domain.append(('account_move', '=', move.id))
+
+        if pos_order_ref:
+            domain.append(('name', '=', pos_order_ref))
+
+        orders = request.env['pos.order'].sudo().search(domain)
+
+        values = {
+            'orders': orders,
+            'date_from': fields.Date.from_string(date_from).strftime('%d/%m/%Y'),
+            'date_to': fields.Date.from_string(date_to).strftime('%d/%m/%Y'),
+            'tanggal_cetak': fields.Date.today().strftime("%d %b %Y"),
+        }
+        return request.render('dev_pos.report_sales_hourly_payment', values)
+
+
+    @http.route('/sales/report/contribution_by_brand', type='http', auth='user', website=True)
+    def portal_sales_report_contribution_by_brand(self, **kw):
+        date_from = kw.get('date_from')
+        date_to = kw.get('date_to')
+        invoice_no = kw.get('invoice_no')
+        pos_order_ref = kw.get('pos_order_ref')
+
+        # Validasi parameter wajib
+        if not date_from or not date_to:
+            return request.not_found()
+
+        domain = [
+            ('date_order', '>=', date_from),
+            ('date_order', '<=', date_to),
+        ]
+
+        if invoice_no:
+            move = request.env['account.move'].sudo().search([('name', '=', invoice_no)], limit=1)
+            if move:
+                domain.append(('account_move', '=', move.id))
+
+        if pos_order_ref:
+            domain.append(('name', '=', pos_order_ref))
+
+        orders = request.env['pos.order'].sudo().search(domain)
+
+        values = {
+            'orders': orders,
+            'date_from': fields.Date.from_string(date_from).strftime('%d/%m/%Y'),
+            'date_to': fields.Date.from_string(date_to).strftime('%d/%m/%Y'),
+            'tanggal_cetak': fields.Date.today().strftime("%d %b %Y"),
+        }
+        return request.render('dev_pos.report_sales_contribution_by_brand', values)
+
+
+    @http.route('/sales/report/contribution_by_category', type='http', auth='user', website=True)
+    def portal_sales_report_contribution_by_category(self, **kw):
+        date_from = kw.get('date_from')
+        date_to = kw.get('date_to')
+        invoice_no = kw.get('invoice_no')
+        pos_order_ref = kw.get('pos_order_ref')
+
+        # Validasi parameter wajib
+        if not date_from or not date_to:
+            return request.not_found()
+
+        domain = [
+            ('date_order', '>=', date_from),
+            ('date_order', '<=', date_to),
+        ]
+
+        if invoice_no:
+            move = request.env['account.move'].sudo().search([('name', '=', invoice_no)], limit=1)
+            if move:
+                domain.append(('account_move', '=', move.id))
+
+        if pos_order_ref:
+            domain.append(('name', '=', pos_order_ref))
+
+        orders = request.env['pos.order'].sudo().search(domain)
+
+        values = {
+            'orders': orders,
+            'date_from': fields.Date.from_string(date_from).strftime('%d/%m/%Y'),
+            'date_to': fields.Date.from_string(date_to).strftime('%d/%m/%Y'),
+            'tanggal_cetak': fields.Date.today().strftime("%d %b %Y"),
+        }
+        return request.render('dev_pos.report_sales_contribution_by_category', values)
+
+
+    
--- dev_pos/reports/sales_report_preview.py
+++ dev_pos/reports/sales_report_preview.py
@@ -30,3 +30,75 @@ class SalesReportDetailPreview(models.TransientModel):
             'target': 'self',
             'url': url,
         }
+    
+    def action_preview_report_spending(self):
+        self.ensure_one()
+        if not self.vit_date_from or not self.vit_date_to:
+            raise UserError("Tidak dapat menampilkan report. Mohon pilih Date From dan Date To")
+        
+        url = f"/sales/report/spending?date_from={self.vit_date_from}&date_to={self.vit_date_to}"
+        return {
+            'type': 'ir.actions.act_url',
+            'target': 'self',
+            'url': url,
+        }
+    
+    def action_preview_report_hourly(self):
+        self.ensure_one()
+        if not self.vit_date_from or not self.vit_date_to:
+            raise UserError("Tidak dapat menampilkan report. Mohon pilih Date From dan Date To")
+        
+        url = f"/sales/report/hourly?date_from={self.vit_date_from}&date_to={self.vit_date_to}"
+        return {
+            'type': 'ir.actions.act_url',
+            'target': 'self',
+            'url': url,
+        }
+    
+    def action_preview_report_hourly_category(self):
+        self.ensure_one()
+        if not self.vit_date_from or not self.vit_date_to:
+            raise UserError("Tidak dapat menampilkan report. Mohon pilih Date From dan Date To")
+        
+        url = f"/sales/report/hourly_category?date_from={self.vit_date_from}&date_to={self.vit_date_to}"
+        return {
+            'type': 'ir.actions.act_url',
+            'target': 'self',
+            'url': url,
+        }
+    
+    def action_preview_report_hourly_payment(self):
+        self.ensure_one()
+        if not self.vit_date_from or not self.vit_date_to:
+            raise UserError("Tidak dapat menampilkan report. Mohon pilih Date From dan Date To")
+        
+        url = f"/sales/report/hourly_payment?date_from={self.vit_date_from}&date_to={self.vit_date_to}"
+        return {
+            'type': 'ir.actions.act_url',
+            'target': 'self',
+            'url': url,
+        }
+    
+    def action_preview_report_contribution_by_brand(self):
+        self.ensure_one()
+        if not self.vit_date_from or not self.vit_date_to:
+            raise UserError("Tidak dapat menampilkan report. Mohon pilih Date From dan Date To")
+        
+        url = f"/sales/report/contribution_by_brand?date_from={self.vit_date_from}&date_to={self.vit_date_to}"
+        return {
+            'type': 'ir.actions.act_url',
+            'target': 'self',
+            'url': url,
+        }
+    
+    def action_preview_report_contribution_by_category(self):
+        self.ensure_one()
+        if not self.vit_date_from or not self.vit_date_to:
+            raise UserError("Tidak dapat menampilkan report. Mohon pilih Date From dan Date To")
+        
+        url = f"/sales/report/contribution_by_category?date_from={self.vit_date_from}&date_to={self.vit_date_to}"
+        return {
+            'type': 'ir.actions.act_url',
+            'target': 'self',
+            'url': url,
+        }
