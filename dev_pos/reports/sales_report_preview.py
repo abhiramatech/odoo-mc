ef73992b29c9008f2359fcecbba1acbf266a24ba
Zulfa <zulfafani@gmail.com>
2025-09-03 14:37:18 +0700

    [IMP] update report preview

--- dev_pos/reports/sales_report_preview.py
+++ dev_pos/reports/sales_report_preview.py
@@ -19,6 +19,20 @@ class SalesReportDetailPreview(models.TransientModel):
             'url': url,
         }
     
+    # def action_preview_report_detail(self):
+    #     self.ensure_one()
+    #     return self.env.ref('dev_pos.action_report_sales_detail').report_action(
+    #         self, data={'date_from': self.vit_date_from, 'date_to': self.vit_date_to}
+    #     )
+    
+    def action_download_pdf(self):
+        self.ensure_one()
+        if not self.vit_date_from or not self.vit_date_to:
+            raise UserError("Tidak dapat mendownload report. Mohon pilih Date From dan Date To")
+
+        # Panggil report QWeb PDF
+        return self.env.ref('dev_pos.report_sales_detail_pdf').report_action(self)
+    
     def action_preview_report_recap(self):
         self.ensure_one()
         if not self.vit_date_from or not self.vit_date_to:
